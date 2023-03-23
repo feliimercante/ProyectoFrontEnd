f3d9221ab4e87ab01d92bb64668970bc1f298a38
feliimercante <felipemercanteorozco@gmail.com>
2023-03-23 19:04:25 -0300

    first commit

---
+++ src/app/model/persona.model.ts
@@ -0,0 +1,12 @@
+export class persona{
+id?: Number;
+nombre: String;
+titulo: String;
+descripcion: String;
+
+constructor(nombre: String, titulo: String, descripcion: String){
+this.nombre = nombre;
+this.titulo = titulo;
+this.descripcion = descripcion;
+}
+}
